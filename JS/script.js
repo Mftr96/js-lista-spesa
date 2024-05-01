0aae7799f55f4cbcdc911641d6f34125f5fabdbe
Mftr96 <matteo.federici96@gmail.com>
2024-05-01 11:36:14 +0200

    milestone3(crezione elementi in pagina)

--- JS/script.js
+++ JS/script.js
@@ -8,6 +8,9 @@ let lista=[
     "pasta"
 ]
 
+//(milestone3)richiamo elemento ul in JS
+let lista_spesa=document.getElementById("lista");
+
 //creo il ciclo for per stampare gli elementi in console
 
 /*
@@ -26,6 +29,14 @@ i=0
 while(i < lista.length){
     const element = lista[i];
     console.log(element);
+    let oggetto_spesa=document.createElement("li");
+    lista_spesa.append(oggetto_spesa);
+    oggetto_spesa.innerText= element;
     i++;
-
 }
+
+
+
+/*ocio a scrivere bene i comandi in console che sennò sbotti  
+come un disgraziato
+      */
