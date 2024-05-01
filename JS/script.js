7352e4926fbc4e0f4376df2455a579a3af3b80ad
Mftr96 <matteo.federici96@gmail.com>
2024-05-01 11:18:54 +0200

    milestone 2(conversione ciclo for in while)

--- JS/script.js
+++ JS/script.js
@@ -9,8 +9,23 @@ let lista=[
 ]
 
 //creo il ciclo for per stampare gli elementi in console
+
+/*
 for (let i = 0; i < lista.length; i++) {
     const element = lista[i];
     console.log(element)
     
+}
+*/
+
+//converto il ciclo appena fatto in while 
+//definisco l'indice fuori dal while 
+i=0
+
+//definisco il ciclo while 
+while(i < lista.length){
+    const element = lista[i];
+    console.log(element);
+    i++;
+
 }
